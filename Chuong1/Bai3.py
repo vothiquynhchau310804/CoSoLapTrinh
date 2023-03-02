89eef0ad8e8ae80a75faba4a9464b8139970eb23
vothiquynhchau310804 <quynhchauvo123@gmail.com>
2023-03-02 23:12:32 +0700

    sua

--- Chuong1/Bai3.py
+++ Chuong1/Bai3.py
@@ -1,1 +1,6 @@
-print('my name is Quynh Chau')
+print('You will be '+ str(int(myAge)+1)+' in a year.')
+print('You will be '+ str(int( '4' )+1)+' in a year.')
+print('You will be '+ str(     5   )   +' in a year.')
+print('You will be '+       '5'        +' in a year.')
+print('You will be 5'                  +' in a year.')
+print('You will be 5 in a year.')
