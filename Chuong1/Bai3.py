eccba0b5849e5cc36f9887b7961e1ae373b495ad
vothiquynhchau310804 <quynhchauvo123@gmail.com>
2023-03-08 22:24:14 +0700

    lan1

--- Chuong1/Bai3.py
+++
@@ -1,5 +0,0 @@
-print('You will be '+ str(int(myAge)+1)+' in a year.')
-print('You will be '+ str(int( '4' )+1)+' in a year.')
-print('You will be '+ str(     5   )   +' in a year.')
-print('You will be '+       '5'        +' in a year.')
-print('You will be 5'                  +' in a year.'
