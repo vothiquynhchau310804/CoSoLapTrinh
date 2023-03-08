bd9b61d400a0d0b93d1435ecc817a9144589b704
vothiquynhchau310804 <quynhchauvo123@gmail.com>
2023-03-09 00:08:14 +0700

    lan1

---
+++ Chuong3/CauTrucFor_BaiTapOnTap/Cau3.2.py
@@ -0,0 +1,9 @@
+n = int(input("Nhap so nguyen n: "))
+a= 0 #số lượng số đã in trên một dòng 
+
+for i in range(1, n+1):
+    print(i, end=" ")
+    a=a+ 1
+    if a == 10:
+        print()
+        a = 0
