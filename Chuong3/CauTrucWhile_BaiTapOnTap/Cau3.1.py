952db2a0c9094c876d8a0dc6a636fe626b6b52e9
vothiquynhchau310804 <quynhchauvo123@gmail.com>
2023-03-09 23:35:46 +0700

    lan 1

--- Chuong3/CauTrucWhile_BaiTapOnTap/Cau3.1.py
+++ Chuong3/CauTrucWhile_BaiTapOnTap/Cau3.1.py
@@ -1,7 +1,4 @@
-while True: #để lặp vô hạn cho đến khi người dùng nhập giá trị hợp lệ.
-    n = int(input("Nhap so nguyen n (1<=n<=50): "))
-    if n<1 or n>50:
-        print("So n khong thuoc mien tren. Yeu cau nhap lai.")
-    else:
-        break #sử dụng break để thoát khỏi vòng lặp.
-print("Ban da nhap so n = ",n)
+n = int(input("Nhap so nguyen n (1 <= n <= 50): "))
+while n < 1 or n > 50:
+    n = int(input("So n khong thuoc mien tren. Yeu cau nhap lai:"))
+print("Bạn đã nhập số n =",n)
