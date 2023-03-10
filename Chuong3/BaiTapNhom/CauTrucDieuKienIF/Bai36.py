437d164b3942681d5acc8c9c52f41bcd8d7bb41b
vothiquynhchau310804 <quynhchauvo123@gmail.com>
2023-03-10 12:00:15 +0700

    lan1

---
+++ Chuong3/BaiTapNhom/CauTrucDieuKienIF/Bai36.py
@@ -0,0 +1,7 @@
+ch=input('Nhap chu cai: ')
+if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
+    print('Chu cai da nhap la mot nguyen am.')
+elif ch=='y':
+    print('Doi khi la nguyen am doi khi la phu am.')
+else:
+    print('Chu cai do la phu am.')
