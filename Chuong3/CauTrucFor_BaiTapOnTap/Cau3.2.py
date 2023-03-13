c999779572201d28188b923ff20e6be23739a611
vothiquynhchau310804 <quynhchauvo123@gmail.com>
2023-03-13 15:36:24 +0700

    2

--- Chuong3/CauTrucFor_BaiTapOnTap/Cau3.2.py
+++ Chuong3/CauTrucFor_BaiTapOnTap/Cau3.2.py
@@ -1,6 +1,5 @@
 n = int(input("Nhap so nguyen n: "))
 a= 0 #số lượng số đã in trên một dòng 
-
 for i in range(1, n+1):
     print(i, end=" ")
     a=a+ 1
---
+++ Luyencode/chuong3.py
@@ -0,0 +1,38 @@
+# num=int(input('nhap so nguyen: '))
+# if num<0:
+#     print('The number is negative.')
+# else:
+#     print('the number is positive.')
+
+# num=int(input('Nhap: '))
+# if num<0:
+#     print('The number is negative.')
+# elif num%2==0:
+#     print('The number is even.')
+# else:
+#     print('The number is odd')
+# yrsOfService=int(input('Nhap y: '))
+# salary=int(input('nhap s: '))
+# bonus=0
+# if yrsOfService<5:
+#     if salary<500:
+#         bonus=100
+#     else:
+#         bonus=200
+# else:
+#     bonus=700
+# print('Bonus amount: ',bonus)
+# import math
+# print('Nhap a,b,c:')
+# a=int(input('a='))
+# b=int(input('b='))
+# c=int(input('c='))
+# d=b*b-4*a*c
+# if d<0:
+#     print('Phuong trinh vo nghiem!!!')
+# elif d==0:
+#     print('Phuong trinh co nghiem kep x1, x2=, (-b/2*a)')
+# else:
+#     print('Phuong trinh co hai nghiem:\n')
+#     print('x1=',(-b+math.sqrt(d))/2*a)
+
