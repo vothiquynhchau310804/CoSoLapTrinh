4ac7b4c31678c2ffa6e6ceea97fc40bc94159f5d
vothiquynhchau310804 <quynhchauvo123@gmail.com>
2023-03-17 13:25:34 +0700

    2

--- Chuong3/BaiTapCaNhan/Bai3.3.py
+++ Chuong3/BaiTapCaNhan/Bai3.3.py
@@ -3,18 +3,18 @@ y=float(input('y='))
 ch=input('Phep toan:')
 if ch=='+':
     K=round(x+y,1)
-    print(x,'+',y,'=',K,sep='')
+    print(x,ch,y,'=',K,sep='')
 elif ch=='-':
     K=round(x-y,1)
-    print(x,'-',y,'=',K,sep='')
+    print(x,ch,y,'=',K,sep='')
 elif ch=='*':
     K=round(x*y,1)
-    print(x,'*',y,'=',K,sep='')
+    print(x,ch,y,'=',K,sep='')
 elif ch=='/':
     if y==0:
         print('Khong hop le')
     else:
         K=round(x/y,1)
-        print(x,'/',y,'=',K,sep='')
+        print(x,ch,y,'=',K,sep='')
 else:
     print('Khong hop le')
