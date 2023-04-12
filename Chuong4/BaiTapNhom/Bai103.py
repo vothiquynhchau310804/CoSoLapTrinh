9c8fe8d9cb76de10cdda3aece042979ac6fbf209
vothiquynhchau310804 <quynhchauvo123@gmail.com>
2023-04-12 22:44:49 +0700

    4

--- Chuong4/BaiTapNhom/Bai103.py
+++ Chuong4/BaiTapNhom/Bai103.py
@@ -5,7 +5,16 @@ def magicDates(day,month,year):
 def xuLy():
     for year in range(1900,2000):
         for month in range(1,13):
-            for day in range(1,32):
+            if month==2:
+                if (year%4==0 and year%100!=0) or year%400==0:
+                    day=29
+                else:
+                    day=28
+            elif month in [1,3,5,7,8,10,12]:
+                day=31
+            else:
+                day=30
+            for day in range(1,day+1):
                 if magicDates(day,month,year):
                     print('{}/{}/{}'.format(day,month,year))
 xuLy()
